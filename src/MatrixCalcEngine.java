ac16163a9ec0888e9104d2902465e78d443631aa
Utkin Artem <solid.utkin@gmail.com>
2018-07-01 13:36:34 +0300

    Add functionality (add operation)

--- src/MatrixCalcEngine.java
+++ src/MatrixCalcEngine.java
@@ -6,7 +6,24 @@ import java.util.Scanner;
 
 public class MatrixCalcEngine {
 
-    void matrixAdd() {}
+    static Matrix matrixAdd(Matrix matrix1, Matrix matrix2) {
+
+        Matrix matrix3;
+        if (matrix1.getColumn() == matrix2.getColumn() && matrix1.getRow() == matrix2.getColumn()) {
+
+            matrix3 = new Matrix(matrix1.getRow(),matrix1.getColumn());
+            for(int i = 0; i < matrix1.getColumn(); i++) {
+                for(int j = 0; j < matrix1.getRow(); j++) {
+                    matrix3.matrixChangeValue(i, j, matrix1.matrixGetValue(i, j) + matrix2.matrixGetValue(i, j));
+                }
+            }
+        } else {
+            matrix3 = null;
+            System.out.println("It's impossible to add different sizes matrices");
+        }
+
+        return matrix3;
+    }
 
     void matrixSubtract() {}
 
@@ -15,9 +32,16 @@ public class MatrixCalcEngine {
     void matrixDivide() {}
 
     public static void main(String[] args) {
-        Matrix m = new Matrix(3, 3);
-        m.matrixInput();
-        m.matrixOutput();
+        Matrix m1 = new Matrix(3, 3);
+        Matrix m2 = new Matrix(3, 3);
+        m1.matrixInput();
+        m1.matrixOutput();
+
+        m2.matrixInput();
+        m2.matrixOutput();
+
+        Matrix m3 = matrixAdd(m1, m2);
+        m3.matrixOutput();
     }
 }
 
@@ -32,6 +56,25 @@ class Matrix {
         this.column = colomn;
     }
 
+    int[][] getMatrix() {
+        return matrix;
+    }
+
+    int getRow() {
+        return row;
+    }
+    int getColumn() {
+        return column;
+    }
+
+    void matrixChangeValue(int row, int column, int value) {
+        matrix[row][column] = value;
+    }
+
+    int matrixGetValue(int row, int column) {
+        return matrix[row][column];
+    }
+
     void matrixInput() {
 
         Scanner in = new Scanner(System.in);
@@ -41,8 +84,6 @@ class Matrix {
                 matrix[i][j] = in.nextInt();
             }
         }
-
-        in.close();
     }
 
     void matrixOutput() {
@@ -52,5 +93,6 @@ class Matrix {
             }
             System.out.println();
         }
+        System.out.println();
     }
 }
