35b8915d6dac740d3ae8e26ada631fd57cbeac81
Utkin Artem <solid.utkin@gmail.com>
2018-07-02 13:12:15 +0300

    Add functionality (3 new operations) and little fixes

--- src/MatrixCalcEngine.java
+++ src/MatrixCalcEngine.java
@@ -9,7 +9,7 @@ public class MatrixCalcEngine {
     static Matrix matrixAdd(Matrix matrix1, Matrix matrix2) {
 
         Matrix matrix3;
-        if (matrix1.getColumn() == matrix2.getColumn() && matrix1.getRow() == matrix2.getColumn()) {
+        if (matrix1.getColumn() == matrix2.getColumn() && matrix1.getRow() == matrix2.getRow()) {
 
             matrix3 = new Matrix(matrix1.getRow(),matrix1.getColumn());
             for(int i = 0; i < matrix1.getColumn(); i++) {
@@ -25,15 +25,60 @@ public class MatrixCalcEngine {
         return matrix3;
     }
 
-    void matrixSubtract() {}
+    static Matrix matrixSubtract(Matrix matrix1, Matrix matrix2) {
 
-    void matrixMultiply() {}
+        Matrix matrix3;
+        if (matrix1.getColumn() == matrix2.getColumn() && matrix1.getRow() == matrix2.getRow()) {
 
-    void matrixDivide() {}
+            matrix3 = new Matrix(matrix1.getRow(),matrix1.getColumn());
+            for(int i = 0; i < matrix1.getColumn(); i++) {
+                for(int j = 0; j < matrix1.getRow(); j++) {
+                    matrix3.matrixChangeValue(i, j, matrix1.matrixGetValue(i, j) - matrix2.matrixGetValue(i, j));
+                }
+            }
+        } else {
+            matrix3 = null;
+            System.out.println("It's impossible to add different sizes matrices");
+        }
+
+        return matrix3;
+    }
+
+    static Matrix matrixMultiplyByNumber(Matrix matrix, int number) {
+        for(int i = 0; i < matrix.getColumn(); i++) {
+            for(int j = 0; j < matrix.getRow(); j++) {
+                matrix.matrixChangeValue(i, j, matrix.matrixGetValue(i, j) * number);
+            }
+        }
+
+        return matrix;
+    }
+
+    static Matrix matrixMultiply(Matrix matrix1, Matrix matrix2) {
+        Matrix matrix3;
+        if (matrix1.getRow() == matrix2.getColumn()) {
+
+            matrix3 = new Matrix(matrix1.getColumn(),matrix2.getRow());
+            for(int i = 0; i < matrix3.getColumn(); i++) {
+                for(int j = 0; j < matrix3.getRow(); j++) {
+                    int sum = 0;
+                    for(int k = 0; k < matrix1.getColumn(); k++) {
+                        sum += matrix1.matrixGetValue(i,k) * matrix2.matrixGetValue(k,j);
+                    }
+                    matrix3.matrixChangeValue(i,j, sum);
+                }
+            }
+        } else {
+            matrix3 = null;
+            System.out.println("The number of row of matrix №1 should be equal to the number of columns of matrix №2");
+        }
+
+        return matrix3;
+    }
 
     public static void main(String[] args) {
-        Matrix m1 = new Matrix(3, 3);
-        Matrix m2 = new Matrix(3, 3);
+        Matrix m1 = new Matrix(2, 2);
+        Matrix m2 = new Matrix(2, 2);
         m1.matrixInput();
         m1.matrixOutput();
 
@@ -41,6 +86,10 @@ public class MatrixCalcEngine {
         m2.matrixOutput();
 
         Matrix m3 = matrixAdd(m1, m2);
+        m3.matrixOutput();
+        m3 = matrixMultiplyByNumber(m1, 2);
+        m3.matrixOutput();
+        m3 = matrixMultiply(m1, m2);
         m3.matrixOutput();
     }
 }
